4593311c53d7432dd5d40244fa1b3c274b1649b4
Vadym Vyshnevskyi <wallian169@gmail.com>
2024-08-28 21:01:38 +0300

    restored initial settings and requirements

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -40,14 +40,12 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "debug_toolbar",
     "cinema",
     "user",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -122,11 +120,6 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
-
-INTERNAL_IPS = [
-    "127.0.0.1",
-    "localhost",
-]
 
 
 # Static files (CSS, JavaScript, Images)
